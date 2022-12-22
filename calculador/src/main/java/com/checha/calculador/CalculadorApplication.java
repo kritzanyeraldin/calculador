f75e2b9f4f3d95507050fc3b67fb6302d5d5b28f
Kritzan Y <kritzan.sullca.e@uni.pe>
2022-12-22 10:05:10 -0500

    proyecto de start.spring.io

---
+++ calculador/src/main/java/com/checha/calculador/CalculadorApplication.java
@@ -0,0 +1,13 @@
+package com.checha.calculador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadorApplication.class, args);
+	}
+
+}
