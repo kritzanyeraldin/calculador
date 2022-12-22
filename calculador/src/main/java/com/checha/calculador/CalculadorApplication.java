82c968d02122f1a6dcf06a07b55a41fdd0a407c4
Kritzan Y <kritzan.sullca.e@uni.pe>
2022-12-22 10:37:00 -0500

    logica

--- calculador/src/main/java/com/checha/calculador/CalculadorApplication.java
+++ calculador/src/main/java/com/checha/calculador/CalculadorApplication.java
@@ -2,12 +2,14 @@ package com.checha.calculador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+@Service
 public class CalculadorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculadorApplication.class, args);
 	}
+	public int sum(int a, int b){ return a+b;}
 
 }
